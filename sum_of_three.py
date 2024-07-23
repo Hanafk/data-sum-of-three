0e5de72cc03672136bf5d4de07ca14b71bbedb61
Hana Fkiri <hana.fkiri13@gmail.com>
2024-07-23 19:08:27 +0200

    Solve first Python exercise of the day

--- sum_of_three.py
+++ sum_of_three.py
@@ -2,6 +2,15 @@
 
 def sum3(first_number, second_number, third_number):
     """
-    Takes three numbers and returns the sum of those numbers
+    Takes three numbers and returns the sum of those numbers.
+
+    Parameters:
+    first_number (int or float): The first number.
+    second_number (int or float): The second number.
+    third_number (int or float): The third number.
+
+    Returns:
+    int or float: The sum of the three numbers.
     """
-    pass  # YOUR CODE HERE
+    result = first_number + second_number + third_number
+    return result
